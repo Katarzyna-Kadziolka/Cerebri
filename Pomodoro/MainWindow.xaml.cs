d7000c3733b2a931f475e8d6e1ce76159bee8aac
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2020-10-03 17:02:41 +0200

    First steps

---
+++ Pomodoro/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Cerebri
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer _timer;
+        TimeSpan _time;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CountDown(object sender, RoutedEventArgs e) {
+            _time = TimeSpan.FromSeconds(5);
+            _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate {
+                Time.Text = _time.ToString("c");
+                if (_time == TimeSpan.Zero) {
+                    _timer.Stop();
+                    StartButton.IsEnabled = true;
+                }
+
+                _time = _time.Add(TimeSpan.FromSeconds(-1));
+            }, Application.Current.Dispatcher);
+
+            _timer.Start();
+            StartButton.IsEnabled=false;
+        }
+    }
+}
